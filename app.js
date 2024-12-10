1bf61e9efc41075a6a78b4c68aea46f91d952dd1
Lulwa Alkanderi <l.alkindri@gmail.com>
2024-12-10 20:28:52 +0300

    day 2

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
-
 const connectDB = require("./database");
-const booksRoutes = require("./apis/recipes/recipes.routes");
-// const authRoutes = require("./apis/auth/auth.routers");
-
 const PORT = 8000;
 
 const app = express();
 
 app.use(express.json());
-
-// app.use("/media", express.static(path.join(__dirname, "media")));
 app.use("/recipes", recipeRoutes);
-// app.use("/auth", authRoutes);
 
 connectDB();
+
 app.listen(PORT, () => {
   console.log(`The application is running on localhost:${PORT}`);
 });
